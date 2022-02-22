d507bc9b469181152f9cec42386a2badfa342f93
Louis <louis.harel@kellogg.northwestern.edu>
2022-02-22 17:06:23 +0000

    sync

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,7 @@
 class ActivitiesController < ApplicationController
   def create
     @activity = Activity.new(params["activity"])
+    @activity.user_id = @current_user.id # logs the user for each new activity
     @activity.save
     redirect_to "/contacts/#{@activity.contact_id}"
   end
